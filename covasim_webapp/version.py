d2cabf3f5be7ce88e44d046e4476d197f1e925a6
cliffckerr <questionably@gmail.com>
2020-11-11 20:21:41 -0800

    update setup procedure

---
+++ covasim_webapp/version.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['__version__', '__versiondate__']
+
+__version__ = '2.0.0'
+__versiondate__ = '2020-11-11'
--- setup.py
+++ setup.py
@@ -1,45 +1,18 @@
 '''
-Covasim installation. Requirements are listed in requirements.txt. There are three
-options:
-    python setup.py develop          # standard install, includes webapp, does not include optional libraries
-    python setup.py develop nowebapp # backend only, no webapp functionality
-    python setup.py develop full     # full install, including optional libraries (NB: these libraries are not available publicly yet)
+Covasim webapp installation. Typical installation is via:
+
+    python setup.py develop
+
+For further information, see the README.
 '''
 
 import os
-import sys
 import runpy
 from setuptools import setup, find_packages
 
-# Load requirements from txt file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-if 'nowebapp' in sys.argv:
-    print('Removing non-essential dependencies -- running as a web application will not work')
-    sys.argv.remove('nowebapp')
-    webapp_reqs = [
-        'scirisweb>=0.17.0',
-        'gunicorn',
-        'plotly',
-        'ipywidgets',
-        'fire', # Not strictly a webapp dependency, but not required for core functionality
-        'statsmodels', # Likewise
-    ]
-    requirements = [req for req in requirements if req not in webapp_reqs]
-
-if 'full' in sys.argv:
-    print('Performing full installation, including optional dependencies')
-    sys.argv.remove('full')
-    full_reqs = [
-        'synthpops',
-        'parestlib'
-    ]
-    requirements.extend(full_reqs)
-
 # Get version
 cwd = os.path.abspath(os.path.dirname(__file__))
-versionpath = os.path.join(cwd, 'covasim', 'version.py')
+versionpath = os.path.join(cwd, 'covasim_webapp', 'version.py')
 version = runpy.run_path(versionpath)['__version__']
 
 # Get the documentation
@@ -58,19 +31,25 @@ CLASSIFIERS = [
 ]
 
 setup(
-    name="covasim",
+    name="covasim_webapp",
     version=version,
     author="Cliff Kerr, Robyn Stuart, Romesh Abeysuriya, Dina Mistry, Lauren George, and Daniel Klein, on behalf of the IDM COVID-19 Response Team",
     author_email="covasim@idmod.org",
-    description="COVID-19 Agent-based Simulator",
+    description="Webapp for Covasim (COVID-19 Agent-based Simulator)",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url='http://covasim.org',
-    keywords=["Covid-19", "coronavirus", "SARS-CoV-2", "stochastic", "agent-based model", "interventions", "epidemiology"],
+    keywords=["Covid-19", "coronavirus", "SARS-CoV-2", "stochastic", "agent-based model", "interventions", "epidemiology", "webapp"],
     platforms=["OS Independent"],
     classifiers=CLASSIFIERS,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requirements
+    install_requires=[
+        'covasim',
+        'scirisweb>=0.17.0',
+        'gunicorn',
+        'plotly',
+        'ipywidgets',
+        ]
 )
 
